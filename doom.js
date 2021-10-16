dd1d5562aad868e7b2f5db4a38fb43a60d6f59c8
Mathews Fernando <mathewsfpereira@gmail.com>
2021-10-16 17:51:09 -0300

    fast af

--- doom.js
+++ doom.js
@@ -87,19 +87,19 @@ function drawCanvas(ptr) {
 
   ctx.putImageData(render_screen, 0, 0);
 
-  if (number_of_draws === 30) {
-    const base64Image = canvas.toDataURL().replace(/(\r\n|\n|\r)/gm, "");
-    console.clear();
-    console.log("");
-    console.log("");
-    console.log("");
-    for (let i = 0; i < 5; i++) {
-      console.log(
-        "%c x\n",
-        `font-size:400px; color: transparent;border-top:90px;background:url(${base64Image}) no-repeat; background-size: contain;`
-      );
-    }
-  }
+  // if (number_of_draws === 30) {
+  //   const base64Image = canvas.toDataURL().replace(/(\r\n|\n|\r)/gm, "");
+  //   console.clear();
+  //   console.log("");
+  //   console.log("");
+  //   console.log("");
+  //   for (let i = 0; i < 5; i++) {
+  //     console.log(
+  //       "%c x\n",
+  //       `font-size:400px; color: transparent;border-top:90px;background:url(${base64Image}) no-repeat; background-size: contain;`
+  //     );
+  //   }
+  // }
 
   ++number_of_draws;
 }
--- glue.js
+++ glue.js
@@ -1,3 +1,4 @@
+const canvas = document.getElementById("screen");
 const forwardKey = (e, type) => {
   const ev = new KeyboardEvent(type, {
     key: e.key,
@@ -6,12 +7,28 @@ const forwardKey = (e, type) => {
   canvas.dispatchEvent(ev);
 };
 
-document.body.addEventListener("keydown", function (e) {
-  forwardKey(e, "keydown");
-});
+document.body.addEventListener("keydown", (e) => forwardKey(e, "keydown"));
+document.body.addEventListener("keyup", (e) => forwardKey(e, "keyup"));
 
-document.body.addEventListener("keyup", function (e) {
-  forwardKey(e, "keyup");
-});
+let shouldClear = true;
+function loop() {
+  const base64Image = canvas.toDataURL().replace(/(\r\n|\n|\r)/gm, "");
+  if (shouldClear) {
+    console.clear();
+  }
 
-const renderConsole = (canvas) => {};
+  for (let i = 0; i < 5; i++) {
+    console.log(
+      "%c X",
+      `font-size:400px;color: transparent;background:url(${base64Image}) no-repeat; background-size: contain;margin-top: 140px;margin-left: 60px;`
+    );
+  }
+  shouldClear = false;
+}
+
+setInterval(() => {
+  loop();
+}, 100);
+setInterval(() => {
+  shouldClear = true;
+}, 3000);
